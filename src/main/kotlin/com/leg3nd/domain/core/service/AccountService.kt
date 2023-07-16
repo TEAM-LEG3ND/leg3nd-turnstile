a9f50a4750260e346b721c3752bdd40524bb7016
d0lim <limdongyoung0@gmail.com>
2023-07-16 12:40:57 +0900

    feat: implement AccountService

---
+++ src/main/kotlin/com/leg3nd/domain/core/service/AccountService.kt
@@ -0,0 +1,15 @@
+package com.leg3nd.domain.core.service
+
+import com.leg3nd.domain.core.model.Account
+import com.leg3nd.domain.ports.api.AccountServicePort
+import com.leg3nd.domain.ports.database.AccountDatabasePort
+
+class AccountService(
+    private val accountDatabasePort: AccountDatabasePort,
+) : AccountServicePort {
+    override suspend fun create(newAccount: Account): String {
+        val createdAccountId = accountDatabasePort.create(newAccount)
+
+        return createdAccountId
+    }
+}
