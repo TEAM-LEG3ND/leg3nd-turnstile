3bca900d3a029c21d3e272483aeafe32fa81698c
d0lim <limdongyoung0@gmail.com>
2023-07-16 11:35:54 +0900

    feat: set namingStrategy to JsonNamingStrategy

--- src/main/kotlin/com/leg3nd/plugins/Serialization.kt
+++ src/main/kotlin/com/leg3nd/plugins/Serialization.kt
@@ -5,10 +5,18 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNamingStrategy
 
+@OptIn(ExperimentalSerializationApi::class)
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(
+            Json {
+                namingStrategy = JsonNamingStrategy.SnakeCase
+            },
+        )
     }
     routing {
         get("/json/kotlinx-serialization") {
