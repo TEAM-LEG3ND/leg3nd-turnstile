014741223dd7af26fa7930283f1a164a41290382
d0lim <limdongyoung0@gmail.com>
2023-07-16 13:05:13 +0900

    feat: add routes to AccountController

--- src/main/kotlin/com/leg3nd/plugins/Routing.kt
+++ src/main/kotlin/com/leg3nd/plugins/Routing.kt
@@ -1,13 +1,30 @@
 package com.leg3nd.plugins
 
+import com.leg3nd.application.AccountController
+import com.leg3nd.application.dto.CreateAccountRequest
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
+    val accountController by inject<AccountController>()
+
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        route("/account") {
+            post {
+                val createAccountRequestDto = call.receive<CreateAccountRequest>()
+                val createdAccountId = accountController.create(createAccountRequestDto)
+
+                call.respondText(createdAccountId)
+                call.respond(HttpStatusCode.Created)
+            }
         }
     }
 }
