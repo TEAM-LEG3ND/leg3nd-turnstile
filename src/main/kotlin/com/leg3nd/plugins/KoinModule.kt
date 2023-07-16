7bcad65d1b1fa886ca30239997f9c1b11c3fd2ac
d0lim <limdongyoung0@gmail.com>
2023-07-16 13:01:25 +0900

    feat: add KoinModule and apply to Application

---
+++ src/main/kotlin/com/leg3nd/plugins/KoinModule.kt
@@ -0,0 +1,19 @@
+package com.leg3nd.plugins
+
+import io.ktor.server.application.*
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+import org.koin.ksp.generated.module
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
+
+@Module
+@ComponentScan("com.leg3nd")
+class KoinModule
+
+fun Application.configureKoin() {
+    install(Koin) {
+        slf4jLogger()
+        modules(KoinModule().module)
+    }
+}
