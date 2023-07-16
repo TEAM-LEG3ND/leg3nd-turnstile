11d99db4ba7f8e9d79c62f7e3a8d15621a003b4e
d0lim <limdongyoung0@gmail.com>
2023-07-16 12:40:26 +0900

    feat: implement simple MongoAccountRepository

---
+++ src/main/kotlin/com/leg3nd/infrastructure/database/mongo/MongoAccountRepository.kt
@@ -0,0 +1,17 @@
+package com.leg3nd.infrastructure.database.mongo
+
+import com.leg3nd.infrastructure.database.mongo.document.AccountDocument
+import kotlinx.coroutines.reactive.awaitSingle
+import org.litote.kmongo.reactivestreams.KMongo
+import org.litote.kmongo.reactivestreams.getCollection
+
+class MongoAccountRepository {
+    private val client = KMongo.createClient()
+    private val database = client.getDatabase("leg3nd-account")
+    private val accountCollection = database.getCollection<AccountDocument>()
+
+    suspend fun create(accountDocument: AccountDocument): String {
+        val createdAccountDocument = accountCollection.insertOne(accountDocument).awaitSingle()
+        return createdAccountDocument.insertedId?.toString() ?: throw Exception("Insert Failed")
+    }
+}
