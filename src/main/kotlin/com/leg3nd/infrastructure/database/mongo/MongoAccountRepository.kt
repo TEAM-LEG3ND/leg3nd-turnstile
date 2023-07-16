307e2a095a52d7710dc8c51143066155ab3c5c1d
d0lim <limdongyoung0@gmail.com>
2023-07-16 20:44:04 +0900

    feat: create MongoAccountRepository at start

--- src/main/kotlin/com/leg3nd/infrastructure/database/mongo/MongoAccountRepository.kt
+++ src/main/kotlin/com/leg3nd/infrastructure/database/mongo/MongoAccountRepository.kt
@@ -2,13 +2,16 @@ package com.leg3nd.infrastructure.database.mongo
 
 import com.leg3nd.infrastructure.database.mongo.document.AccountDocument
 import kotlinx.coroutines.reactive.awaitFirstOrNull
+import org.koin.core.annotation.Property
 import org.koin.core.annotation.Single
 import org.litote.kmongo.reactivestreams.KMongo
 import org.litote.kmongo.reactivestreams.getCollection
 
-@Single
-class MongoAccountRepository {
-    private val client = KMongo.createClient()
+@Single(createdAtStart = true)
+class MongoAccountRepository(
+    @Property("mongo.uri") mongoUri: String,
+) {
+    private val client = KMongo.createClient(mongoUri)
     private val database = client.getDatabase("leg3nd-account")
     private val accountCollection = database.getCollection<AccountDocument>()
 
