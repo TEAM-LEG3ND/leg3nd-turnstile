a894f37723f94d13ab3f419904e9befb21007d3f
d0lim <limdongyoung0@gmail.com>
2023-07-16 13:01:05 +0900

    feat: configure Koin with KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,20 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val kmongo_version: String by project
+val koin_version: String by project
+val koin_ktor_version: String by project
+val koin_ksp_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.0"
     id("io.ktor.plugin") version "2.3.2"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
+    id("com.google.devtools.ksp") version "1.9.0-1.0.11"
 }
+
+// Use KSP Plugin
+apply(plugin = "com.google.devtools.ksp")
 
 group = "com.leg3nd"
 version = "0.0.1"
@@ -33,4 +40,15 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 
     implementation("org.litote.kmongo:kmongo-coroutine:$kmongo_version")
+
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_ktor_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_ktor_version")
+    implementation("io.insert-koin:koin-annotations:$koin_ksp_version")
+    ksp("io.insert-koin:koin-ksp-compiler:$koin_ksp_version")
+}
+
+// KSP - To use generated sources
+sourceSets.main {
+    java.srcDirs("build/generated/ksp/main/kotlin")
 }
