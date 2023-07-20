7f398b4fb084f24e2d74069b87615ff15d6d13cf
d0lim <limdongyoung0@gmail.com>
2023-07-20 21:37:41 +0900

    feat: add services to Account domain as sealed class list

--- src/main/kotlin/com/leg3nd/domain/core/model/Account.kt
+++ src/main/kotlin/com/leg3nd/domain/core/model/Account.kt
@@ -9,9 +9,35 @@ data class Account(
     var fullName: String,
     var oAuthProvider: OAuthProvider,
     var status: Status = Status.DRAFT,
+    var services: List<Service>,
     val createdAt: LocalDateTime = LocalDateTime.now(),
     var updatedAt: LocalDateTime = LocalDateTime.now(),
 ) {
+    sealed class Service(
+        val type: ServiceType,
+    ) {
+        data class Studium(
+            val nickname: String,
+            val profileImageUrl: String,
+            var status: Status,
+            val intro: String,
+            val createdAt: LocalDateTime = LocalDateTime.now(),
+            var updatedAt: LocalDateTime = LocalDateTime.now(),
+        ) : Service(ServiceType.STUDIUM)
+
+        data class BreadN(
+            val nickname: String,
+            val profileImageUrl: String,
+            var status: Status,
+            val createdAt: LocalDateTime = LocalDateTime.now(),
+            var updatedAt: LocalDateTime = LocalDateTime.now(),
+        ) : Service(ServiceType.BREAD_N)
+
+        enum class ServiceType {
+            STUDIUM, BREAD_N
+        }
+    }
+
     enum class OAuthProvider {
         GOOGLE, GITHUB
     }
