5e219b8ea3b2872ea4eea129d4509cbdf555da01
d0lim <limdongyoung0@gmail.com>
2023-07-20 23:02:07 +0900

    feat: add addService method

--- src/main/kotlin/com/leg3nd/domain/core/service/AccountService.kt
+++ src/main/kotlin/com/leg3nd/domain/core/service/AccountService.kt
@@ -3,15 +3,29 @@ package com.leg3nd.domain.core.service
 import com.leg3nd.domain.core.model.Account
 import com.leg3nd.domain.ports.api.AccountServicePort
 import com.leg3nd.domain.ports.database.AccountDatabasePort
+import io.ktor.server.plugins.*
 import org.koin.core.annotation.Single
+import org.slf4j.LoggerFactory
 
 @Single
 class AccountService(
     private val accountDatabasePort: AccountDatabasePort,
 ) : AccountServicePort {
+    private val log = LoggerFactory.getLogger(this::class.java)
+
     override suspend fun create(newAccount: Account): String {
         val createdAccountId = accountDatabasePort.create(newAccount)
 
         return createdAccountId
+    }
+
+    override suspend fun addService(accountId: String, service: Account.Service) {
+        val account = accountDatabasePort.findById(accountId).getOrElse {
+            log.error("error occurred when findById", it)
+            throw BadRequestException("No such user with account id")
+        }
+
+        val newServices = account.services + listOf(service)
+        accountDatabasePort.updateServicesById(accountId, newServices)
     }
 }
