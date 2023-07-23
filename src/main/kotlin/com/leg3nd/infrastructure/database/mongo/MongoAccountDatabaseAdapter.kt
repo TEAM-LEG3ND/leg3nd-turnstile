f8a4e7c1119194dfc75fba50643c2e2e7410c534
d0lim <limdongyoung0@gmail.com>
2023-07-24 00:04:04 +0900

    fix: findById caused by object id

--- src/main/kotlin/com/leg3nd/infrastructure/database/mongo/MongoAccountDatabaseAdapter.kt
+++ src/main/kotlin/com/leg3nd/infrastructure/database/mongo/MongoAccountDatabaseAdapter.kt
@@ -3,11 +3,13 @@ package com.leg3nd.infrastructure.database.mongo
 import com.leg3nd.domain.core.model.Account
 import com.leg3nd.domain.ports.database.AccountDatabasePort
 import com.leg3nd.infrastructure.database.mongo.document.AccountDocument
-import kotlinx.coroutines.reactive.awaitFirst
-import kotlinx.coroutines.reactive.awaitFirstOrNull
+import org.bson.types.ObjectId
 import org.koin.core.annotation.Property
 import org.koin.core.annotation.Single
+import org.litote.kmongo.Id
+import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.eq
+import org.litote.kmongo.id.toId
 import org.litote.kmongo.reactivestreams.KMongo
 import org.litote.kmongo.reactivestreams.getCollection
 import org.litote.kmongo.setValue
@@ -18,22 +20,23 @@ class MongoAccountDatabaseAdapter(
     @Property("mongo.uri") mongoUri: String,
 ) : AccountDatabasePort {
 
-    private val client = KMongo.createClient(mongoUri)
+    private val client = KMongo.createClient(mongoUri).coroutine
     private val database = client.getDatabase("leg3nd-account")
     private val accountCollection = database.getCollection<AccountDocument>()
 
     override suspend fun create(newAccount: Account): String {
         val newAccountDocument = AccountDocument.fromDomain(newAccount)
 
-        accountCollection.insertOne(newAccountDocument).awaitFirstOrNull()
-            ?: throw Exception("account insert Failed")
+        accountCollection.insertOne(newAccountDocument)
 
         return newAccountDocument._id.toString()
     }
 
     override suspend fun findById(id: String): Result<Account> = runCatching {
-        val documentId = id.toId<AccountDocument>()
-        val accountDocument = accountCollection.find(AccountDocument::_id eq documentId).awaitFirst()
+        val documentId = id.toObjectId()
+
+        val accountDocument = accountCollection.findOneById(documentId)
+            ?: throw NoSuchElementException("Account Document not found with id $documentId")
 
         accountDocument.toDomain()
     }
@@ -45,6 +48,9 @@ class MongoAccountDatabaseAdapter(
         accountCollection.findOneAndUpdate(
             AccountDocument::_id eq documentId,
             setValue(AccountDocument::services, documentServices),
-        ).awaitFirst()
+        )
     }
+
+    private fun String.toObjectId(): Id<AccountDocument> =
+        ObjectId(this).toId()
 }
