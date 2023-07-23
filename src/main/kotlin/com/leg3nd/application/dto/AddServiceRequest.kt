6b18d4d30d4fe392f84999437e1d358e846d0e87
d0lim <limdongyoung0@gmail.com>
2023-07-24 00:04:19 +0900

    feat: add Serializable to AddServiceRequest

--- src/main/kotlin/com/leg3nd/application/dto/AddServiceRequest.kt
+++ src/main/kotlin/com/leg3nd/application/dto/AddServiceRequest.kt
@@ -1,5 +1,8 @@
 package com.leg3nd.application.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class AddServiceRequest(
     val accountId: String,
     val serviceType: String,
