e78174b09a016fd5a981329e93e0e395c8345e42
d0lim <limdongyoung0@gmail.com>
2023-08-01 23:39:26 +0900

    feat: apply jwt routing

--- src/main/kotlin/com/leg3nd/plugins/Routing.kt
+++ src/main/kotlin/com/leg3nd/plugins/Routing.kt
@@ -1,10 +1,16 @@
 package com.leg3nd.plugins
 
 import com.leg3nd.application.AccountController
+import com.leg3nd.application.AuthController
 import com.leg3nd.application.dto.AddServiceRequest
 import com.leg3nd.application.dto.CreateAccountRequest
+import com.leg3nd.application.dto.OAuthLoginRequest
+import com.leg3nd.application.dto.TokenResponse
+import com.leg3nd.domain.core.model.Account
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.plugins.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -12,11 +18,30 @@ import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
+    val authController by inject<AuthController>()
     val accountController by inject<AccountController>()
 
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        route("/auth") {
+            route("/login") {
+                post("/google") {
+                    val oAuthLoginRequest = call.receive<OAuthLoginRequest>()
+                    val tokenPair = authController.login(Account.OAuthProvider.GOOGLE, oAuthLoginRequest)
+
+                    call.response.cookies.append(
+                        // TODO: add more cookie setting for example secure and httpOnly
+                        Cookie(
+                            name = "Refresh",
+                            value = tokenPair.refreshToken,
+                        ),
+                    )
+                    call.respond(TokenResponse.fromTokenPair(tokenPair))
+                }
+            }
         }
 
         route("/account") {
@@ -40,6 +65,18 @@ fun Application.configureRouting() {
                 accountController.addService(addServiceRequestDto)
 
                 call.respond(HttpStatusCode.Created)
+            }
+        }
+
+        authenticate("auth-jwt") {
+            route("/auth") {
+                get("/me") {
+                    val principal = call.principal<JWTPrincipal>()
+                    val accountId = principal!!.payload.getClaim("id").asString()
+
+                    val account = accountController.getAccountById(accountId)
+                    call.respond(account)
+                }
             }
         }
     }
