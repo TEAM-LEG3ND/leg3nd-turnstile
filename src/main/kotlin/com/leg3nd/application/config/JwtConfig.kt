52b2ce58b0ed5557cc009f4d16c4fc2ccdabad02
d0lim <limdongyoung0@gmail.com>
2023-08-01 23:32:13 +0900

    feat: add JwtConfig

---
+++ src/main/kotlin/com/leg3nd/application/config/JwtConfig.kt
@@ -0,0 +1,18 @@
+package com.leg3nd.application.config
+
+import com.leg3nd.domain.core.service.JwtServiceConfig
+import org.koin.core.annotation.Property
+import org.koin.core.annotation.Single
+import kotlin.time.Duration
+
+@Single
+class JwtConfig(
+    @Property("jwt.secret") override val secret: String,
+    @Property("jwt.refreshSecret") override val refreshSecret: String,
+    @Property("jwt.audience") override val audience: String,
+    @Property("jwt.duration") durationRaw: String,
+    @Property("jwt.refreshDuration") refreshDurationRaw: String,
+) : JwtServiceConfig {
+    override val duration: Duration = Duration.parse(durationRaw)
+    override val refreshDuration: Duration = Duration.parse(refreshDurationRaw)
+}
