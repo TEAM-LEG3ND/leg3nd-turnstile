9f7bccbbceb042f26f1ce90506f5d76a11530a9c
d0lim <limdongyoung0@gmail.com>
2023-08-13 12:58:26 +0900

    fix: OAuthService to implement OAuthServicePort

--- src/main/kotlin/com/leg3nd/domain/core/service/OAuthService.kt
+++ src/main/kotlin/com/leg3nd/domain/core/service/OAuthService.kt
@@ -3,6 +3,7 @@ package com.leg3nd.domain.core.service
 import com.leg3nd.domain.core.model.Account
 import com.leg3nd.domain.core.model.OAuthUser
 import com.leg3nd.domain.ports.client.OAuthClientPort
+import com.leg3nd.domain.ports.service.OAuthServicePort
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
@@ -11,8 +12,8 @@ import org.koin.core.annotation.Single
 @Single
 class OAuthService(
     private val oAuthClientPort: OAuthClientPort,
-) {
-    suspend fun loginWithOAuth(oAuthProvider: Account.OAuthProvider, authorizationCode: String): OAuthUser =
+) : OAuthServicePort {
+    override suspend fun loginWithOAuth(oAuthProvider: Account.OAuthProvider, authorizationCode: String): OAuthUser =
         coroutineScope {
             val oAuthTokenDeferred =
                 async(Dispatchers.IO) { oAuthClientPort.getOAuthToken(oAuthProvider, authorizationCode) }
