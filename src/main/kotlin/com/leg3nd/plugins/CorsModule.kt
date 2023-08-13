bb074579b879045bbc07fc77c04a97535e7034e7
d0lim <limdongyoung0@gmail.com>
2023-08-13 13:46:02 +0900

    fix: allow x- headers to cors

--- src/main/kotlin/com/leg3nd/plugins/CorsModule.kt
+++ src/main/kotlin/com/leg3nd/plugins/CorsModule.kt
@@ -14,6 +14,8 @@ fun Application.configureCors() {
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.ContentType)
         allowHeader(HttpHeaders.Authorization)
+        allowHeader("x-service-type")
+        allowHeader("x-endpoint")
         allowCredentials = true
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
