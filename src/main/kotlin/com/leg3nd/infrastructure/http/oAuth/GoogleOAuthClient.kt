6e4a79991b9047139037bc32884e5ee9cca404e8
d0lim <limdongyoung0@gmail.com>
2023-09-04 23:40:31 +0900

    refactor: try logging

--- src/main/kotlin/com/leg3nd/infrastructure/http/oAuth/GoogleOAuthClient.kt
+++ src/main/kotlin/com/leg3nd/infrastructure/http/oAuth/GoogleOAuthClient.kt
@@ -7,12 +7,14 @@ import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonNamingStrategy
 import org.koin.core.annotation.Property
 import org.koin.core.annotation.Single
+import org.slf4j.LoggerFactory
 
 @Single
 class GoogleOAuthClient(
@@ -20,6 +22,8 @@ class GoogleOAuthClient(
     @Property("oAuth.google.clientSecret") private val googleClientSecret: String,
     @Property("oAuth.google.redirectUri") private val googleRedirectUri: String,
 ) : OAuthClient {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     @OptIn(ExperimentalSerializationApi::class)
     private val client = HttpClient(CIO) {
@@ -41,7 +45,11 @@ class GoogleOAuthClient(
             "&grant_type=authorization_code" +
             "&code=$code"
 
-        return client.post(url).body<OAuthTokenResponse>()
+        val res = client.post(url)
+
+        log.error(res.bodyAsText())
+
+        return res.body<OAuthTokenResponse>()
     }
 
     override suspend fun getOAuthUser(accessToken: String): OAuthUserResponse {
